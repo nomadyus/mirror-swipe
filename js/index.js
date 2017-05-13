3ab3bf717413a426d247f29aa717a79523f70e0d
yusuf-kami <yusuf.kami@gmail.com>
2017-05-13 12:08:43 -0400

    adding sound effects

--- js/index.js
+++ js/index.js
@@ -1,19 +1,48 @@
 (function () {
-  $(document).ready(preparePage);  
-  
+  $(document).ready(preparePage);
+
+  var rateUpAudio, rateDownAudio;
+
+  function addAudioSounds() {
+    rateUpAudio = new Audio('../aud/star5.mp3');
+    rateDownAudio = new Audio('../aud/star1.mp3');
+  }
+
   function addEventListenerToPage() {
-    $('#rate-up').on('click', function(a, b) {
+    $('#rate-up').on('click', function (a, b) {
       console.log('Rate up!');
+      stopRateDownSound();
+      playRateUpSound();
     });
-    
-    $('#rate-down').on('click', function(a, b) {
+
+    $('#rate-down').on('click', function (a, b) {
       console.log('Rate down!');
-    })
+      stopRateUpSound();
+      playRateDownSound();
+    });
+  }
+
+  function playRateUpSound() {
+    rateUpAudio.play();
+  }
+
+  function playRateDownSound() {
+    rateDownAudio.play();
+  }
+
+  function stopRateUpSound() {
+    rateUpAudio.pause();
+    rateUpAudio.currentTime = 0;
+  }
+
+  function stopRateDownSound() {
+    rateDownAudio.pause();
+    rateDownAudio.currentTime = 0;
   }
 
   function preparePage() {
-    console.log('Yeaaah!')
+    console.log('Yeaaah!');
+    addAudioSounds();
     addEventListenerToPage();
-    
   }
 })();
